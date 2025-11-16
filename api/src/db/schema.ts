979e706264e60e02d5af7a08132d5558ca418b25
ted <theo_drz@live.com>
2025-11-15 23:05:46 -0500

    created api routes for signup and login

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -1,13 +1,21 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
-  id: integer('id').primaryKey(),
-  email: text('email').notNull(),
+  id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
+  email: text('email').unique().notNull(),
   passwordHash: text('passwordHash').notNull(),
-  passwordSalt: text('passwordSalt').notNull(),
   date: integer('date').notNull(),
   vault: text('vault').notNull(),
 });
 
 export type UsersInsert = typeof users.$inferInsert;
 export type UsersSelect = typeof users.$inferSelect;
+
+export const sessions = sqliteTable('sessions', {
+  id: integer('id').notNull(),
+  date: integer('date').notNull(),
+  token: text('token').primaryKey().unique().notNull(),
+});
+
+export type SessionsInsert = typeof sessions.$inferInsert;
+export type SessionsSelect = typeof sessions.$inferSelect;
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,13 +1,82 @@
 import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import { db } from './db/index.js';
+import { sessions, users } from './db/schema.js';
+import { eq } from 'drizzle-orm';
 
 const router = Router();
 
-router.post('/signup', (_, res) => {
-  res.send('signup route');
+type LoginCredentials = {
+  email?: string,
+  password?: string,
+}
+
+// function validateLoginCredentials(creds: LoginCredentials) {
+//   return creds.email && creds.password
+// }
+
+router.post<{}, any, LoginCredentials>('/signup', async (req, res) => {
+  let { email, password } = req.body;
+
+  // FIX ME
+  // validate email is a valid email and password is valid (min length, has some special chars, etc..)
+  // should also send confirmation/validation email to address, only actually add the account once confirmed
+  if (!email || !password) return res.sendStatus(400);
+
+  email = email.trim().toLowerCase();
+
+  const emailAlreadyExists = await db.select().from(users).where(
+    eq(users.email, email)
+  ).get();
+  if (emailAlreadyExists) return res.sendStatus(409);
+
+  const passwordHash = await bcrypt.hash(password, 12);
+
+  await db.insert(users).values({
+    email,
+    passwordHash,
+    date: Date.now(),
+    vault: '',
+  });
+
+  res.sendStatus(201);
 });
 
-router.post('/login', (_, res) => {
-  res.send('login route');
+router.post<{}, any, LoginCredentials>('/login', async (req, res) => {
+  let { email, password } = req.body;
+
+  if (!email || !password) return res.sendStatus(400);
+
+  const userRecord = await db.select().from(users).where(
+    eq(users.email, email)
+  ).get();
+
+  if (!userRecord) return res.status(401).json('Invalid email or password');
+
+  const passwordMatch = await bcrypt.compare(
+    password,
+    userRecord.passwordHash
+  );
+
+  if (!passwordMatch) return res.status(401).json('Invalid email or password');
+
+  const sessionToken = crypto.randomBytes(32).toString('hex');
+
+  await db.insert(sessions).values({
+    id: userRecord.id,
+    token: sessionToken,
+    date: Date.now(),
+  });
+
+  res.cookie('sessionToken', sessionToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    path: '/refresh',
+  });
+
+  res.sendStatus(201);
 });
 
 router.post('/logout', (_, res) => {
